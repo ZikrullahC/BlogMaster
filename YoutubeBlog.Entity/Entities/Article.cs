2ffcf39507254e156fb1b23546c532f0e35f00be
ZikrullahC <=>
2024-10-15 11:43:48 +0300

    Added entity layer with initial models and configurations

---
+++ YoutubeBlog.Entity/Entities/Article.cs
@@ -0,0 +1,18 @@
+﻿using YoutubeBlog.Core.Entities;
+
+namespace YoutubeBlog.Entity.Entities
+{
+    public class Article : EntityBase
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int ViewCount { get; set; }
+
+        //Id degerini EF Core otomatik olarak sinifin ismine gore ayarlar. Mesela CategoryId degeri Category sinifina aittir.
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public Guid ImageId { get; set; }
+        public Image Image { get; set; }
+    }
+}
